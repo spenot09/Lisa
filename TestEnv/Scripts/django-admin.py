4f262a074f1fa21fe68449b6b0b43c0361b55f76
danielcheng1035 <43863202+danielcheng1035@users.noreply.github.com>
2018-12-11 16:31:16 +0000

    Login Test
    Basic login features.

---
+++ TestEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Anaconda3\envs\TestEnv\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ TestEnv/website/website/views.py
@@ -0,0 +1,3 @@
+from django.http import HttpResponse
+def placeholder(request):
+    return HttpResponse("test")
